bc311e63d1bf56c8eb20e304e83574ce278de75d
Mariane Simão <mariane.mato@portalsesisp.org.br>
2023-11-14 13:53:24 -0300

    commit 1411

--- Controllers/PedidoController.cs
+++ Controllers/PedidoController.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using nihongo2.Context;
 using nihongo2.Models;
 using nihongo2.Repositories.Interfaces;
 
@@ -12,10 +14,12 @@ namespace nihongo2.Controllers
     {
         private readonly IPedidoRepository _pedidoR;
         private readonly Carrinho _carrinho;
-        public PedidoController(IPedidoRepository pedidoR, Carrinho carrinho)
+        private readonly AppDbContext _context;
+        public PedidoController(IPedidoRepository pedidoR, Carrinho carrinho, AppDbContext context)
         {
             _pedidoR = pedidoR;
             _carrinho = carrinho;
+            _context = context;
         }
         public IActionResult Checkout()
         {
@@ -53,10 +57,31 @@ namespace nihongo2.Controllers
                 ViewBag.TotalPedido = _carrinho.GetCarrinhoCompraTotal();
                 //limpa o carrinho do cliente
                 _carrinho.LimparCarrinho();
+                ViewBag.UrlWhatsApp = EnviarMensagem(pedido.PedidoId);
                 //exibe a view com dados do cliente e do pedido
                 return View("~/Views/Pedido/CheckoutCompleto.cshtml", pedido);
             }
             return View(pedido);
+        }
+        public string EnviarMensagem(int pedidoId)
+        {
+            var pedido = _context.Pedidos.Include(p =>
+        p.PedidoItens).ThenInclude(p => p.Item).FirstOrDefault(p => p.PedidoId ==
+        pedidoId);
+
+            string numeroDestinatario = "+55Digite o DDD e numero tudo junto";
+
+string mensagem = "Pedido: " + pedido.PedidoId.ToString() + "Cliente: "+pedido.Nome.ToString();
+
+// Formate o número removendo caracteres não numéricos
+string numeroFormatado = new string(numeroDestinatario.Where(char.IsDigit).ToArray());
+
+            // Construa o link do WhatsApp
+            string urlWhatsApp = $"https://web.whatsapp.com/send?phone=+{numeroFormatado}&text={Uri.EscapeDataString(mensagem)}";
+
+            Console.WriteLine(urlWhatsApp);
+            // Retorne a View que contém o script JavaScript
+            return urlWhatsApp;
         }
     }
 }
